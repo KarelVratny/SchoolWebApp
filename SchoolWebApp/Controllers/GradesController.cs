8e148d659583c5c4408532930e111bddc99eeddb
KarelVratny <karelvratny@gmail.com>
2024-06-05 10:39:25 +0200

    Grade Controller and Create View

---
+++ SchoolWebApp/Controllers/GradesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolWebApp.Services;
+
+namespace SchoolWebApp.Controllers {
+    public class GradesController : Controller {
+        private GradeService _gradeService;
+
+        public GradesController(GradeService gradeService) {
+            _gradeService = gradeService;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
