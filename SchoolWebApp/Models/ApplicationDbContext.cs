bee7a334ba53ebcba982c7f5533b91a98a3a6508
KarelVratny <karelvratny@gmail.com>
2024-06-05 10:13:22 +0200

    added Grade

--- SchoolWebApp/Models/ApplicationDbContext.cs
+++ SchoolWebApp/Models/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
 namespace SchoolWebApp.Models {
-	public class ApplicationDbContext : DbContext {
-		public DbSet<Student> Students { get; set; }
-		public DbSet<Subject> Subjects { get; set; }
-		public ApplicationDbContext(DbContextOptions options) : base(options) {
-		}
-	}
+    public class ApplicationDbContext : DbContext {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options) {
+        }
+    }
 }
--- SchoolWebApp/Models/Subject.cs
+++ SchoolWebApp/Models/Subject.cs
@@ -1,6 +1,9 @@
-﻿namespace SchoolWebApp.Models {
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SchoolWebApp.Models {
 	public class Subject {
 		public int Id { get; set; }
+		[NotNull]
 		public string Name { get; set; }
 	}
 }
