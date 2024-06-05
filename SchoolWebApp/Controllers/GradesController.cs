9f27f9debd3454b5759cb1bfc89a21e0b2a110b9
KarelVratny <karelvratny@gmail.com>
2024-06-05 14:47:39 +0200

    Grade Update+Delete

--- SchoolWebApp/Controllers/GradesController.cs
+++ SchoolWebApp/Controllers/GradesController.cs
@@ -15,11 +15,16 @@ namespace SchoolWebApp.Controllers {
 		//	return View();
 		//}
 		public async Task<IActionResult> CreateAsync() {
+			await FillSelectsAsync();
+			return View();
+		}
+
+		private async Task FillSelectsAsync() {
 			var gradesDropdowsData = await _gradeService.GetGradesDropdowsData();
 			ViewBag.Students = new SelectList(gradesDropdowsData.Students, "Id", "LastName");
 			ViewBag.Subjects = new SelectList(gradesDropdowsData.Subjects, "Id", "Name");
-			return View();
 		}
+
 		[HttpPost]
 		public async Task<IActionResult> Create(GradeDTO gradeDTO) {
 			await _gradeService.CreateAsync(gradeDTO);
@@ -29,6 +34,28 @@ namespace SchoolWebApp.Controllers {
 		public async Task<IActionResult> Index() {
 			var allGrades = await _gradeService.GetAllViewModelsAsync();
 			return View(allGrades);
+		}
+
+		public async Task<IActionResult> Update(int id) {
+			var gradeToEdit = await _gradeService.GetByIdAsync(id);
+			if (gradeToEdit == null) {
+				return View("NotFound");
+			}
+			var gradeDto = _gradeService.ModelToDto(gradeToEdit);
+			await FillSelectsAsync();
+			return View(gradeDto);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Update(int id, GradeDTO grade) {
+			await _gradeService.UpdateAsync(id, grade);
+			return RedirectToAction("Index");
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> DeleteAsync(int id) {
+			await _gradeService.DeleteAsync(id);
+			return RedirectToAction("Index");
 		}
 	}
 }
--- SchoolWebApp/Services/GradeService.cs
+++ SchoolWebApp/Services/GradeService.cs
@@ -18,32 +18,64 @@ namespace SchoolWebApp.Services {
 			};
 		}
 
-		internal async Task CreateAsync(GradeDTO gradeDTO) {
-			Grade gradeToInsert = new Grade() {
+		internal async Task CreateAsync(GradeDTO gradeDto) {
+			Grade gradeToInsert = await DtoToModel(gradeDto);
+			await _context.AddAsync(gradeToInsert);
+			await _context.SaveChangesAsync();
+		}
+
+		private async Task<Grade> DtoToModel(GradeDTO gradeDTO) {
+			return new Grade() {
+				Id = gradeDTO.Id,
 				Date = DateTime.Today,
 				Mark = gradeDTO.Mark,
 				Topic = gradeDTO.Topic,
 				Student = await _context.Students.FirstOrDefaultAsync(student => student.Id == gradeDTO.StudentId),
 				Subject = await _context.Subjects.FirstOrDefaultAsync(subject => subject.Id == gradeDTO.SubjectId),
 			};
-			await _context.AddAsync(gradeToInsert);
-			await _context.SaveChangesAsync();
 		}
 
 		public async Task<IEnumerable<GradesViewModel>> GetAllViewModelsAsync() {
-			List<Grade> grades = await _context.Grades.Include(grade=>grade.Student).Include(grade => grade.Subject).ToListAsync();
-			List<GradesViewModel> gradeViewModels =new List<GradesViewModel>();
+			List<Grade> grades = await _context.Grades.Include(grade => grade.Student).Include(grade => grade.Subject).ToListAsync();
+			List<GradesViewModel> gradeViewModels = new List<GradesViewModel>();
 			foreach (Grade grade in grades) {
 				gradeViewModels.Add(new GradesViewModel {
 					Date = grade.Date,
 					Id = grade.Id,
 					Mark = grade.Mark,
-					StudentName=$"{grade.Student.LastName} {grade.Student.FirstName}",
-					SubjectName=grade.Subject.Name,
-					Topic=grade.Topic
+					StudentName = $"{grade.Student.LastName} {grade.Student.FirstName}",
+					SubjectName = grade.Subject.Name,
+					Topic = grade.Topic
 				});
 			}
 			return gradeViewModels;
+		}
+
+		internal async Task<Grade> GetByIdAsync(int id) {
+			return await _context.Grades.Include(grade => grade.Student).Include(grade => grade.Subject).FirstOrDefaultAsync(grade => grade.Id == id);
+		}
+
+		internal GradeDTO ModelToDto(Grade grade) {
+			return new GradeDTO {
+				Id = grade.Id,
+				Date = grade.Date,
+				Mark = grade.Mark,
+				StudentId = grade.Student.Id,
+				SubjectId = grade.Subject.Id,
+				Topic = grade.Topic
+			};
+		}
+
+		internal async Task UpdateAsync(int id, GradeDTO gradeDto) {
+			Grade gradeToUpdate = await DtoToModel(gradeDto);
+			_context.Grades.Update(gradeToUpdate);
+			await _context.SaveChangesAsync();
+		}
+
+		internal async Task DeleteAsync(int id) {
+			var gradeToDelete = await _context.Grades.FirstOrDefaultAsync(grade => grade.Id == id);
+			_context.Grades.Remove(gradeToDelete);
+			_context.SaveChanges();
 		}
 	}
 }
