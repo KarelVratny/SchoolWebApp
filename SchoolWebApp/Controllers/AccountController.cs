750ba9758bf2e6f573552c50754768cd20719597
KarelVratny <karelvratny@gmail.com>
2024-06-07 14:43:05 +0200

    Login/Logout

---
+++ SchoolWebApp/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SchoolWebApp.Models;
+using SchoolWebApp.ViewModels;
+
+namespace SchoolWebApp.Controllers {
+    [Authorize]
+    public class AccountController : Controller {
+        private UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager) {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl) {
+            LoginViewModel loginViewModel = new LoginViewModel();
+            loginViewModel.ReturnUrl = returnUrl;
+            return View(loginViewModel);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel login) {
+            if (ModelState.IsValid) {
+                AppUser appUser = await _userManager.FindByNameAsync(login.Username);
+                if (appUser != null) {
+                    await _signInManager.SignOutAsync();
+                    Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(appUser, login.Password, false, false);
+                    if (result.Succeeded) {
+                        return Redirect(login.ReturnUrl ?? "/");
+                    }
+                }
+                ModelState.AddModelError(nameof(login.Username), "Login Failed: Invalid UserName or password");
+            }
+            return View(login);
+        }
+
+        public async Task<IActionResult> Logout() {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- SchoolWebApp/Controllers/HomeController.cs
+++ SchoolWebApp/Controllers/HomeController.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SchoolWebApp.Models;
 using System.Diagnostics;
@@ -5,13 +7,16 @@ using System.Diagnostics;
 namespace SchoolWebApp.Controllers {
     public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
+        private UserManager<AppUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger) {
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager) {
             _logger = logger;
+            _userManager = userManager;
         }
-
-        public IActionResult Index() {
-            return View();
+        [Authorize]
+        public async Task<IActionResult> Index() {
+            AppUser signedInUser = await _userManager.GetUserAsync(HttpContext.User);
+            return View("Index", signedInUser.UserName);
         }
 
         public IActionResult Privacy() {
