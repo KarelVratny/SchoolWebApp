d178990d7cc58b982984ca79c8ed20e45e3985da
KarelVratny <karelvratny@gmail.com>
2024-06-07 13:20:57 +0200

    Create, Update, Delete User

---
+++ SchoolWebApp/Controllers/UsersController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using SchoolWebApp.Models;
+using SchoolWebApp.ViewModels;
+
+namespace SchoolWebApp.Controllers {
+    public class UsersController : Controller {
+        private UserManager<AppUser> _userManager;
+        private IPasswordHasher<AppUser> _passwordHasher;
+
+        public UsersController(UserManager<AppUser> userManager, IPasswordHasher<AppUser> passwordHasher) {
+            _userManager = userManager;
+            _passwordHasher = passwordHasher;
+        }
+
+        public IActionResult Index() {
+            return View(_userManager.Users);
+        }
+
+        public ActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(UserViewModel userViewModel) {
+            if (ModelState.IsValid) {
+                AppUser newUser = new AppUser() {
+                    UserName = userViewModel.Name,
+                    Email = userViewModel.Email,
+                };
+                IdentityResult result = await _userManager.CreateAsync(newUser, userViewModel.Password);
+                if (result.Succeeded) {
+                    return RedirectToAction("Index");
+                }
+                else {
+                    //foreach (var error in result.Errors) {
+                    //    ModelState.AddModelError("", error.Description);
+                    //}
+                    AddErrors(result);
+                }
+            }
+            return View(userViewModel);
+        }
+
+        public async Task<IActionResult> Update(string id) {
+            AppUser userToEdit = await _userManager.FindByIdAsync(id);
+            if (userToEdit == null) {
+                return View("NotFound");
+            }
+            return View(userToEdit);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateAsync(string id, string email, string password) {
+            AppUser userToEdit = await _userManager.FindByIdAsync(id);
+            if (userToEdit != null) {
+                //if (!string.IsNullOrWhiteSpace(email)) {
+                //    userToEdit.Email = email;
+                //}
+                //else {
+                //    ModelState.AddModelError("", "Email cannot be empty");
+                //}
+                //if (!string.IsNullOrWhiteSpace(password)) {
+                //    userToEdit.PasswordHash = _passwordHasher.HashPassword(userToEdit, password);
+                //}
+                //else {
+                //    ModelState.AddModelError("", "Password cannto be empty");
+                //}
+                if (!string.IsNullOrWhiteSpace(email) && !string.IsNullOrEmpty(password)) {
+                    IdentityResult identityResult = await _userManager.UpdateAsync(userToEdit);
+                    if (identityResult.Succeeded) {
+                        return RedirectToAction("Index");
+                    }
+                    else {
+                        AddErrors(identityResult);
+                    }
+                }
+            }
+            else {
+                ModelState.AddModelError("", "User not found");
+            }
+            return View(userToEdit);
+        }
+
+        private void AddErrors(IdentityResult identityResult) {
+            foreach (var error in identityResult.Errors) {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id) {
+            AppUser? userToDelete = await _userManager.FindByIdAsync(id);
+            if (userToDelete != null) {
+                var result = await _userManager.DeleteAsync(userToDelete);
+                if (result.Succeeded) {
+                    return RedirectToAction("Index");
+                }
+                else {
+                    AddErrors(result);
+                }
+            }
+            else {
+                ModelState.AddModelError("", "User not found");
+            }
+            return View("Index", _userManager.Users);
+        }
+    }
+}
