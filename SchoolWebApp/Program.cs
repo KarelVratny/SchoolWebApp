37bbcb6c25998bc910fea42133ca516945c48989
KarelVratny <karelvratny@gmail.com>
2024-06-12 14:19:35 +0200

    input type password for passwords

--- SchoolWebApp/Program.cs
+++ SchoolWebApp/Program.cs
@@ -39,6 +39,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Home/Error");
+    // Pro zobrazeni chyb na publishovane appce
+    //app.UseDeveloperExceptionPage();
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
