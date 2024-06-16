ebbc9a39b3df4ab9d80588745926d0f8666d0d85
KarelVratny <karelvratny@gmail.com>
2024-06-16 18:21:02 +0200

    Privacy page and logout button in navbar

--- SchoolWebApp/Program.cs
+++ SchoolWebApp/Program.cs
@@ -11,9 +11,9 @@ builder.Services.AddControllersWithViews();
 
 //Connection string
 builder.Services.AddDbContext<ApplicationDbContext>(options => {
-    //options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolDbConnection"));
-	//options.UseSqlServer(builder.Configuration.GetConnectionString("AzureDbConnection"));
-	options.UseSqlServer(builder.Configuration.GetConnectionString("MonsterDbConnection"));	
+	//options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolDbConnection"));
+	options.UseSqlServer(builder.Configuration.GetConnectionString("AzureDbConnection"));
+	//options.UseSqlServer(builder.Configuration.GetConnectionString("MonsterDbConnection"));
 });
 
 //Predani kontroleru instanci service
@@ -22,27 +22,27 @@ builder.Services.AddScoped<SubjectService>();
 builder.Services.AddScoped<GradeService>();
 builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.Configure<IdentityOptions>(options => {
-    options.Password.RequireDigit = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequiredLength = 8;
-    options.Password.RequireUppercase = true;
-    options.Password.RequireLowercase = true;
+	options.Password.RequireDigit = false;
+	options.Password.RequireNonAlphanumeric = false;
+	options.Password.RequiredLength = 8;
+	options.Password.RequireUppercase = true;
+	options.Password.RequireLowercase = true;
 });
 builder.Services.ConfigureApplicationCookie(options => {
-    options.Cookie.Name = ".AspNetCore.Identity.Application";
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(1);
-    options.SlidingExpiration = true;
+	options.Cookie.Name = ".AspNetCore.Identity.Application";
+	options.ExpireTimeSpan = TimeSpan.FromMinutes(1);
+	options.SlidingExpiration = true;
 });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()) {
-    app.UseExceptionHandler("/Home/Error");
-    // Pro zobrazeni chyb na publishovane appce
-    //app.UseDeveloperExceptionPage();
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	// Pro zobrazeni chyb na publishovane appce
+	//app.UseDeveloperExceptionPage();
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -54,7 +54,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
